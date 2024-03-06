6598d1696a89a27249ee3f56131ac4484c985741
sungjun son <o_nj_u@naver.com>
2024-03-06 23:15:13 +0900

    20240306 bodyParser limit 10MB (해결안됨)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClipboardController } from './clipboard/clipboard.controller';
 import { ClipboardService } from './clipboard/clipboard.service';
 import { Clipboard } from './clipboard/clipboard.entity';
+import * as bodyParser from 'body-parser';
 
 @Module({
   imports: [
@@ -25,4 +26,11 @@ import { Clipboard } from './clipboard/clipboard.entity';
   controllers: [ClipboardController],
   providers: [ClipboardService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // bodyParser 미들웨어의 limit 옵션 설정
+    consumer
+      .apply(bodyParser.json({ limit: '10mb' })) // 최대 10MB까지 허용
+      .forRoutes('*'); // 모든 라우트에 대해 적용
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // bodyParser 미들웨어의 limit 옵션 설정
+  app.use(bodyParser.json({ limit: '10mb' }));
   await app.listen(3000);
 }
 bootstrap();
